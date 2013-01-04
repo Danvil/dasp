a5b1d41b88cd13068a2bcae94eea58f51132c468
David Weikersdorfer <davidw@danvil.de>
2013-01-04 20:37:29 +0100

    minor

--- dasv_vis/main.cpp
+++ dasv_vis/main.cpp
@@ -17,7 +17,6 @@ int main(int argc, char *argv[])
 	po::options_description desc("Allowed options");
 	desc.add_options()
 		("help", "produce help message")
-		("live", "Kinect live mode")
 		("rgbd_mode", po::value(&p_rgbd_mode)->default_value(p_rgbd_mode), "rgbd stream mode (test, static, oni, live)")
 		("rgbd_arg", po::value(&p_rgbd_arg)->default_value(p_rgbd_arg), "rgbd stream argument")
 		("rgbd_seek", po::value(&p_rgbd_seek)->default_value(p_rgbd_seek), "rgbd stream seek (only usable with mode=oni)")
