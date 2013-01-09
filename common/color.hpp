a0134cce03f4f4d37caa6a5640b4ae0163276361
David Weikersdorfer <davidw@danvil.de>
2013-01-09 13:10:57 +0100

    Added color generators

--- common/color.hpp
+++ common/color.hpp
@@ -16,6 +16,21 @@ inline Eigen::Vector3f SimilarityColor(float x)
 	return {color.r,color.g,color.b};
 }
 
+inline Eigen::Vector3f IntensityColor(float x)
+{
+	static auto cm = Danvil::ContinuousIntervalColorMapping<float, float>::Factor_Black_Blue_Red_Yellow_White();
+	cm.setRange(0.0f, 1.0f);
+	Danvil::Colorf color = cm(x);
+	return {color.r,color.g,color.b};
+}
+
+inline Eigen::Vector3f CountColor(int num, int min, int max)
+{
+	return IntensityColor(
+		static_cast<float>(num - min)/static_cast<float>(max-min)
+	);
+}
+
 //----------------------------------------------------------------------------//
 }
 //----------------------------------------------------------------------------//
