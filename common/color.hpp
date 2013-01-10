449c1b53ce0955b2efe05f4ad651643974b0adb1
David Weikersdorfer <davidw@danvil.de>
2013-01-10 11:14:29 +0100

    Added depth to color

--- common/color.hpp
+++ common/color.hpp
@@ -1,8 +1,9 @@
-#ifndef PLOTTING_HPP_
-#define PLOTTING_HPP_
+#ifndef COMMON_COLOR_HPP_
+#define COMMON_COLOR_HPP_
 //----------------------------------------------------------------------------//
 #include <Danvil/Color.h>
 #include <Eigen/Dense>
+#include <Slimage/Slimage.hpp>
 //----------------------------------------------------------------------------//
 namespace common {
 //----------------------------------------------------------------------------//
@@ -24,11 +25,31 @@ inline Eigen::Vector3f IntensityColor(float x)
 	return {color.r,color.g,color.b};
 }
 
-inline Eigen::Vector3f CountColor(int num, int min, int max)
+template<typename T>
+inline Eigen::Vector3f CountColor(T num, T min, T max)
 {
 	return IntensityColor(
-		static_cast<float>(num - min)/static_cast<float>(max-min)
+		(num < min)
+			? 0.0f
+			: static_cast<float>(num - min)/static_cast<float>(max-min)
 	);
+}
+
+inline slimage::Pixel3ub ColorToPixel(const Eigen::Vector3f& color) {
+	return slimage::Pixel3ub{{
+		static_cast<unsigned char>(255.f*std::min(1.0f, std::max(0.0f, color[0]))),
+		static_cast<unsigned char>(255.f*std::min(1.0f, std::max(0.0f, color[1]))),
+		static_cast<unsigned char>(255.f*std::min(1.0f, std::max(0.0f, color[2])))
+	}};
+}
+
+inline slimage::Image3ub ColorizeDepth(const slimage::Image1ui16& img16, uint16_t min, uint16_t max) {
+	slimage::Image3ub img(img16.width(), img16.height());
+	const int n = img16.size();
+	for(int i=0; i<n; i++) {
+		img[i] = ColorToPixel(CountColor((uint16_t)img16[i], min, max));
+	}
+	return img;
 }
 
 //----------------------------------------------------------------------------//
