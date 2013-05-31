7de890b63d5b2357f5c2183886464d19f8d9f526
David Weikersdorfer <davidw@danvil.de>
2013-05-31 18:28:50 +0200

    Added tool for density-adaptive smoothing

---
+++ density_cmd/main.cpp
@@ -0,0 +1,43 @@
+#include <density/PointDensity.hpp>
+#include <boost/program_options.hpp>
+#include <boost/timer/timer.hpp>
+#include <algorithm>
+#include <iostream>
+#include <fstream>
+#include <cmath>
+
+int main(int argc, char** argv)
+{
+	std::string p_in = "";
+	std::string p_out = "pnts.tsv";
+
+	namespace po = boost::program_options;
+	po::options_description desc;
+	desc.add_options()
+		("help", "produce help message")
+		("density", po::value(&p_in), "density function image (leave empty for test function)")
+		("out", po::value(&p_out), "filename of result file with samples points")
+	;
+
+	po::variables_map vm;
+	po::store(po::parse_command_line(argc, argv, desc), vm);
+	po::notify(vm);
+	if(vm.count("help")) {
+		std::cerr << desc << std::endl;
+		return 1;
+	}
+
+	Eigen::MatrixXf rho = density::LoadDensity(p_in);
+	std::cout << "Loaded density dim=" << rho.rows() << "x" << rho.cols() << ", sum=" << rho.sum() << "." << std::endl;
+
+	Eigen::MatrixXf result;
+	{
+		boost::timer::auto_cpu_timer t;
+		result = density::DensityAdaptiveSmooth(rho);
+	}
+
+	density::SaveDensity(p_out, result);
+	std::cout << "Wrote result to file '" << p_out << "'." << std::endl;
+
+	return 1;
+}
