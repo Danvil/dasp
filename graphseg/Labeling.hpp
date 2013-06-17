b7c290e6e8b927d78c0e5840bd625ab80bccce9b
David Weikersdorfer <davidw@danvil.de>
2013-06-17 16:06:21 +0200

    Added new ucm label method which uses hystheresis and can join and split supervised labels

--- graphseg/Labeling.hpp
+++ graphseg/Labeling.hpp
@@ -131,42 +131,137 @@ namespace graphseg
 		return GraphLabeling::CreateClean(cluster_labels);
 	}
 
-	template<typename Graph, typename EdgeWeightMap, typename VertexLabelMap>
-	inline std::vector<int> ComputeSegmentLabels_UCM_Supervised(
-		const Graph& graph,
-		EdgeWeightMap edge_weight_map,
-		VertexLabelMap vertex_label_map,
-		float threshold,
-		bool split_unconnected)
+	namespace impl
 	{
-		const std::size_t num_vertices = boost::num_vertices(graph);
-		// find maximal used label
-		int label_max = -1;
-		for(auto vid : as_range(boost::vertices(graph))) {
-			const int label = boost::get(vertex_label_map, vid);
-			label_max = std::max(label_max, label);
-		}
-		const int label_supervised_threshold = label_max;
-		// labeled clusters keep label, unlabeled clusters get new label
-		// labels will indicate if clusters are supervised:
-		//   is_supervised(c) := (label(c) <= label_supervised_threshold)
-		std::vector<int> cluster_labels(num_vertices);
-		std::map<int, unsigned int> label_count;
-		for(auto vid : as_range(boost::vertices(graph))) {
-			int label = boost::get(vertex_label_map, vid);
-			if(label < 0) {
-				// unsupervised cluster -> need label
-				label = ++label_max;
-			}
-			cluster_labels[vid] = label;
-			label_count[label] ++;
-		}
-		// get all edges
 		struct Edge
 		{
 			unsigned int a, b;
 			float weight;
 		};
+
+		/** Prepare labels
+		 * Assigns new labels to unsupervised vertices.
+		 * Returns the maximal label for supervised vertices.
+		 */
+		template<typename Graph, typename VertexLabelMap>
+		std::vector<int> PrepareLabels(
+			const Graph& graph, VertexLabelMap vertex_label_map,
+			int* supervised_max_id
+		) {
+			// find maximal supervised label
+			int label_max = -1;
+			for(auto vid : as_range(boost::vertices(graph))) {
+				const int label = boost::get(vertex_label_map, vid);
+				label_max = std::max(label_max, label);
+			}
+			*supervised_max_id = label_max;
+			// give unsupervised vertices a unique label
+			std::vector<int> v_labels(boost::num_vertices(graph));
+			for(auto vid : as_range(boost::vertices(graph))) {
+				int label = boost::get(vertex_label_map, vid);
+				if(label < 0) {
+					// unsupervised cluster -> need label
+					label = ++label_max;
+				}
+				v_labels[vid] = label;
+			}
+			return v_labels;
+		}
+
+		inline
+		void MergeLabels(std::vector<int>& labels, int a, int b) {
+			// keep the smaller label (this is the potentially supervised label)
+			if(a < b) {
+				std::swap(a, b);
+			}
+			// join segments connected by edge
+			std::replace(labels.begin(), labels.end(), a, b);
+		}
+	}
+
+	/** Supervised labeling
+	 * All edges with a weight lower than the threshold are merged.
+	 * Edges are not merged if they connect clusters which already have
+	 * a label which is not identical for both vertices. 
+	 */
+	template<typename Graph, typename EdgeWeightMap, typename VertexLabelMap>
+	inline std::vector<int> ComputeSegmentLabels_UCM_Supervised(
+		const Graph& graph,
+		EdgeWeightMap edge_weight_map,
+		VertexLabelMap vertex_label_map,
+		float threshold)
+	{
+		// find supervised maximal label and give unique labels to unsupervised vertices
+		int label_supervised_threshold;
+		std::vector<int> cluster_labels = impl::PrepareLabels(graph, vertex_label_map, &label_supervised_threshold);
+		// get all edges with a weight smaller than the threshold
+		typedef impl::Edge Edge;
+		std::vector<Edge> edges;
+		edges.reserve(boost::num_edges(graph));
+		for(auto eid : as_range(boost::edges(graph))) {
+			float w = boost::get(edge_weight_map, eid);
+			if(w < threshold) {
+				edges.push_back(Edge{
+					static_cast<unsigned int>(boost::source(eid, graph)),
+					static_cast<unsigned int>(boost::target(eid, graph)),
+					w
+				});
+			}
+		}
+		// sort edges by weight
+		std::sort(edges.begin(), edges.end(), [](const Edge& x, const Edge& y) { return x.weight < y.weight; });
+		// cut one edge after another and merge labels
+		for(const Edge& edge : edges) {
+			// get cluster labels
+			const int label_a = cluster_labels[edge.a];
+			const int label_b = cluster_labels[edge.b];
+			if(label_a == label_b) {
+				// clusters are already part of the same segment -> nothing to do
+				continue;
+			}
+			// check if cluster segments are supervised
+			const bool is_supervised_a = (label_a <= label_supervised_threshold);
+			const bool is_supervised_b = (label_b <= label_supervised_threshold);
+			// do not merge if both clusters are supervised
+			if(is_supervised_a && is_supervised_b) {
+				continue;
+			}
+			// merge labels
+			impl::MergeLabels(cluster_labels, label_a, label_b);
+		}
+
+		// return raw labels
+		return cluster_labels;
+	}
+
+	namespace impl
+	{
+
+		struct ucm_sd_debug {
+			std::vector<Edge> force_merge;
+			std::vector<Edge> normal_merge;
+			std::vector<Edge> reject_merge;
+			std::vector<Edge> keep_merge;
+			std::vector<Edge> no_merge;
+		};
+	}
+
+	/** Dynamic supervised labeling
+	 * Similar to Supervised Labeling, but can also change supervised labels.
+	 */
+	template<typename Graph, typename EdgeWeightMap, typename VertexLabelMap>
+	inline std::vector<int> ComputeSegmentLabels_UCM_Supervised_Dynamic(
+		const Graph& graph,
+		EdgeWeightMap edge_weight_map,
+		VertexLabelMap vertex_label_map,
+		float th_low, float th_normal, float th_high,
+		impl::ucm_sd_debug* dbg=0)
+	{
+		// find supervised maximal label and give unique labels to unsupervised vertices
+		int label_supervised_threshold;
+		std::vector<int> cluster_labels = impl::PrepareLabels(graph, vertex_label_map, &label_supervised_threshold);
+		// get all edges
+		typedef impl::Edge Edge;
 		std::vector<Edge> edges;
 		edges.reserve(boost::num_edges(graph));
 		for(auto eid : as_range(boost::edges(graph))) {
@@ -178,101 +273,125 @@ namespace graphseg
 		}
 		// sort edges by weight
 		std::sort(edges.begin(), edges.end(), [](const Edge& x, const Edge& y) { return x.weight < y.weight; });
-		// collect uncut edges
+		// collect merged edges
 		std::vector<Edge> merged_edges;
 		// cut one edge after another and merge clusters into segments
-		for(unsigned int k=0; k<edges.size(); k++) {
-			const Edge& edge = edges[k];
-			// merge only if edge weight is smaller than thresold
-			if(edge.weight >= threshold) {
-				// all consecutive edges will have larger edge weights -> break
-				break;
-			}
-			// get cluster labels
-			const int label_a = cluster_labels[edge.a];
-			const int label_b = cluster_labels[edge.b];
-			if(label_a == label_b) {
-				// clusters are already part of the same segment -> nothing to do
-				merged_edges.push_back(edge);
-				continue;
-			}
-			// check if cluster segments are supervised
+		for(const Edge& edge : edges) {
+			// get edge data
+			int& label_a = cluster_labels[edge.a];
+			int& label_b = cluster_labels[edge.b];
 			const bool is_supervised_a = (label_a <= label_supervised_threshold);
 			const bool is_supervised_b = (label_b <= label_supervised_threshold);
-			// compute label merge
-			int old_label = label_a;
-			int new_label = label_b;
-			if(is_supervised_a && is_supervised_b) {
-				// do not merge if both clusters are supervised
-				continue;
-				// // keep the label which has the higher occurence count
-				// if(label_count[old_label] > label_count[new_label]) {
-				// 	std::swap(old_label, new_label);
-				// }
+			const float edge_weight = edge.weight;
+			// check low weight
+			if(edge_weight < th_low) {
+				// always merge edge
+				merged_edges.push_back(edge);
+				if(dbg) dbg->force_merge.push_back(edge);
+				// merge labels now only if not both are supervised
+				// when merging supervised labels we need to consider more ...
+				if(!(is_supervised_a && is_supervised_b)) {
+					impl::MergeLabels(cluster_labels, label_a, label_b);
+				}
+			}
+			// check normal weight
+			else if(edge_weight < th_normal) {
+				// merge edges and labels now if at least one cluster is not supervised
+				if(label_a == label_b || !(is_supervised_a && is_supervised_b)) {
+					merged_edges.push_back(edge);
+					if(dbg) dbg->normal_merge.push_back(edge);
+					if(label_a != label_b) {
+						impl::MergeLabels(cluster_labels, label_a, label_b);
+					}
+				}
+				else {
+					if(dbg) dbg->reject_merge.push_back(edge);
+				}
+			}
+			// check high weight
+			else if(edge_weight < th_high) {
+				// merge edge if same label
+				if(label_a == label_b) {
+					merged_edges.push_back(edge);
+					if(dbg) dbg->keep_merge.push_back(edge);
+				}
+				else {
+					if(dbg) dbg->no_merge.push_back(edge);
+				}
 			}
 			else {
-				// keep the smaller label
-				// this assures that the label supervision property is guaranteed
-				if(old_label < new_label) {
-					std::swap(old_label, new_label);
-				}
+				if(dbg) dbg->no_merge.push_back(edge);
 			}
-			// join segments connected by edge
-			std::replace(cluster_labels.begin(), cluster_labels.end(), old_label, new_label);
-			merged_edges.push_back(edge);
 		}
-
-		if(split_unconnected) {
-			// compute connected componentes
-			// create a graph will all edges with costs >= threshold
-			SpectralGraph cropped(boost::num_vertices(graph));
-			for(const Edge& e : merged_edges) {
-				auto p = boost::add_edge(e.a, e.b, cropped);
-				cropped[p.first] = e.weight;
-			}
-
-			// compute connected components
-			std::vector<int> cluster_labels_components(boost::num_vertices(cropped));
-			unsigned int num_labels = boost::connected_components(cropped, &cluster_labels_components[0]);
-			std::cout << "CC# = " << num_labels << std::endl;
-			// check if two components have the same label index
-			std::map<int, std::map<unsigned int, unsigned int>> component_count_per_label;
-			for(unsigned int i=0; i<cluster_labels.size(); i++) {
-				int label = cluster_labels[i];
-				unsigned int cmp_id = cluster_labels_components[i];
-				component_count_per_label[label][cmp_id] ++;
-			}
-			for(auto p : component_count_per_label) {
-				if(p.second.size() > 1) {
-					//std::cout << "Label " << p.first << ": " << std::endl;
-					//std::cout << "\tcmp\tcnt" << std::endl;
-					unsigned int max_cmp = 0;
-					unsigned int max_num = 0;
-					for(auto q : p.second) {
-						if(q.second >= max_num) {
-							max_cmp = q.first;
-							max_num = q.second;
-						}
+		// compute label for new clusters
+		int label_max = *std::max_element(cluster_labels.begin(), cluster_labels.end());
+		label_max = std::max(label_max, label_supervised_threshold);
+		// construct a graph with all merged edges
+		SpectralGraph cropped(boost::num_vertices(graph));
+		for(const Edge& e : merged_edges) {
+			auto p = boost::add_edge(e.a, e.b, cropped);
+			cropped[p.first] = e.weight;
+		}
+		// compute connected components
+		std::vector<int> cluster_labels_components(boost::num_vertices(cropped));
+		unsigned int num_labels = boost::connected_components(cropped, &cluster_labels_components[0]);
+		// std::cout << "CC# = " << num_labels << std::endl;
+		// check if two components have the same label index
+		std::map<int, std::map<unsigned int, unsigned int>> label_component_count;
+		for(unsigned int i=0; i<cluster_labels.size(); i++) {
+			int label = cluster_labels[i];
+			unsigned int cmp_id = cluster_labels_components[i];
+			label_component_count[label][cmp_id] ++;
+		}
+		for(auto p : label_component_count) {
+			if(p.second.size() > 1) {
+				// std::cout << "Label " << p.first << ": " << std::endl;
+				// std::cout << "\tcmp\tcnt" << std::endl;
+				unsigned int max_cmp = 0;
+				unsigned int max_num = 0;
+				for(auto q : p.second) {
+					if(q.second >= max_num) {
+						max_cmp = q.first;
+						max_num = q.second;
 					}
-					std::map<unsigned int, int> label_remap;
-					for(auto q : p.second) {
-						if(q.first != max_cmp) {
-							label_remap[q.first] = ++label_max;
-						}
-						else {
-							label_remap[q.first] = p.first;
-						}
-						//std::cout << "\t" << q.first << "\t" << q.second << "\t->" << label_remap[q.first] << std::endl;
+				}
+				std::map<unsigned int, int> label_remap;
+				for(auto q : p.second) {
+					if(q.first != max_cmp) {
+						label_remap[q.first] = ++label_max;
 					}
-					for(unsigned int i=0; i<cluster_labels.size(); i++) {
-						if(cluster_labels[i] == p.first) {
-							cluster_labels[i] = label_remap[cluster_labels_components[i]];
-						}
+					else {
+						label_remap[q.first] = p.first;
+					}
+					// std::cout << "\t" << q.first << "\t" << q.second << "\t->" << label_remap[q.first] << std::endl;
+				}
+				for(unsigned int i=0; i<cluster_labels.size(); i++) {
+					if(cluster_labels[i] == p.first) {
+						cluster_labels[i] = label_remap[cluster_labels_components[i]];
 					}
 				}
 			}
 		}
-
+		// check if one component contains more than one label
+		std::map<int, std::map<unsigned int, unsigned int>> component_label_count;
+		for(unsigned int i=0; i<cluster_labels.size(); i++) {
+			int label = cluster_labels[i];
+			unsigned int cmp_id = cluster_labels_components[i];
+			component_label_count[cmp_id][label] ++;
+		}
+		for(auto p : component_label_count) {			
+			unsigned int max_label = 0;
+			unsigned int max_num = 0;
+			for(auto q : p.second) {
+				if(q.second >= max_num) {
+					max_label = q.first;
+					max_num = q.second;
+				}
+			}
+			for(auto q : p.second) {
+				impl::MergeLabels(cluster_labels, q.first, max_label);
+			}
+		}		
 		// return raw labels
 		return cluster_labels;
 	}
